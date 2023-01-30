2c056d341f13123321ce490293a8df87a9136ca8
admin <admin@YD05-07>
2023-01-30 17:51:57 +0900

    new

--- Assigment/src/assignment/assignment2.java
+++ Assigment/src/assignment/assignment2.java
@@ -14,20 +14,39 @@ public class assignment2 {
 		// 제3사분면 : x<0, y<0
 		// 제4사분면 : x>0, y<0
 		// 문제출처, 백준(https://www.acmicpc.net/) 14681번 문제
+		System.out.println("x의 값을 넣으시오");
+		int x = Integer.parseInt(scanner.nextLine());
+		System.out.println("y의 값을 넣으시오");
+		int y = Integer.parseInt(scanner.nextLine());
 		
-
+		if(x>0 && y>0) {
+			System.out.println("제 1사분면: "+ x +">0, "+y+">0");			
+		}else if(x<0 && y>0) {
+			System.out.println("제 2사분면: "+ x +"<0, "+y+">0");	
+		}else if(x<0 && y<0) {
+			System.out.println("제 3사분면: "+ x +"<0, "+y+"<0");
+		}else {
+			System.out.println("제 4사분면: "+ x +">0, "+y+"<0");
+		}
 		// 문제2) 연도가 주어졌을 때 해당 년도가 윤년인지를 확인해서 출력하도록 하세요.
 		// 윤년은 연도가 4의 배수이면서 100의 배수가 아닐 때 또는 400의 배수일때입니다.
 		// 예를 들어, 2012년은 4의 배수이면서 100의 배수가 아니라서 윤년이며,
 		// 1900년은 100의 배수이고 400의 배수는 아니기 때문에 윤년이 아닙니다.
 		// HiNT : 이중 IF문 사용
 		// 문제출처, 백준(https://www.acmicpc.net/) 2753번 문제
+		System.out.println("연도의 값을 넣으시오");
+		int year = Integer.parseInt(scanner.nextLine());
+		if(year%4==0 ? year%100==0 : year%400==0) {
+			System.out.println(year+"년은 윤년이 아닙니다");
+		}else {
+			System.out.println(year+"년은 윤년입닙니다");
+		}
 		
 		// 문제3) switch문을 이용하여 가위, 바위, 보 중 하나가 주어졌을 때 사용자 어떤 값을 가져야 이길 수 있는 지를 출력하도록
 		// 구현하세요.
 		// 예를 들어, 가위가 주어졌을 때 "이기기 위해선 바위를 내야합니다." 라고 출력하도록 하세요.
 		// 한 단어를 입력받을 경우 Scanner객체의 next() 함수를 사용하면 됩니다.
-
+		
 		// 문제4) 차례대로 m과 n을 입력받아 m단을 n번째까지 출력하도록 하세요.
 		// 예를 들어 2와 3을 입력받았을 경우 아래처럼 출력합니다.
 		// 2 X 1 = 2
